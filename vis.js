aa56b975a2f864a663df0bd12cc7fd9d1d611bb9
JJ-2820 <68925021+JJ-2820@users.noreply.github.com>
2020-07-28 23:15:44 +0100

    Update vis.js

--- vis.js
+++ vis.js
@@ -1,6 +1,6 @@
-/* window.onload = function() {
+window.onload = function() {
 	resizeApp();
-}; */
+};
 
 const visLayer = document.getElementById("cnvLayer1");
 const uiLayer = document.getElementById("cnvLayer2");
